e80e2c623fe0a2bf1729d98aed26243afee3e819
DevDRocky <polaris.d.max@gmail.com>
2017-06-17 14:16:13 +0900

    Default Project Commit

---
+++ app/src/main/java/nobehind/noback/godseyes/HomeView.java
@@ -0,0 +1,13 @@
+package nobehind.noback.godseyes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_view);
+    }
+}
