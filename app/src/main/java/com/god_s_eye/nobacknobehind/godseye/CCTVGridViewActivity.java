b70a7b11528e97c46646232993b0cbe1ab3d938f
BbCs1217 <suni1217g@gmail.com>
2017-06-22 22:32:33 +0900

    add 4 emptu activities

---
+++ app/src/main/java/com/god_s_eye/nobacknobehind/godseye/CCTVGridViewActivity.java
@@ -0,0 +1,17 @@
+package com.god_s_eye.nobacknobehind.godseye;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by suni1 on 2017-06-22.
+ */
+
+public class CCTVGridViewActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cctvgridview);
+    }
+}
