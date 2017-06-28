a140c9fc2c3d9e226e8443fa4737b18223dd6bd0
BbCs1217 <suni1217g@gmail.com>
2017-06-28 22:31:52 +0900

    Daum Map API connect

--- app/src/main/java/com/god_s_eye/nobacknobehind/godseye/AddCCTVActivity.java
+++ app/src/main/java/com/god_s_eye/nobacknobehind/godseye/AddCCTVActivity.java
@@ -3,6 +3,10 @@ package com.god_s_eye.nobacknobehind.godseye;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapView;
 
 /**
  * Created by suni1 on 2017-06-22.
@@ -13,5 +17,17 @@ public class AddCCTVActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcctv);
+
+        InitMapView();
+    }
+
+    private void InitMapView()
+    {
+        //다음지도
+        MapView mapView = new MapView(this);
+        mapView.setDaumMapApiKey("22d40639040389f49922077b56dc2f6a");
+
+        RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
--- app/src/main/java/com/god_s_eye/nobacknobehind/godseye/MainActivity.java
+++ app/src/main/java/com/god_s_eye/nobacknobehind/godseye/MainActivity.java
@@ -1,7 +1,10 @@
 package com.god_s_eye.nobacknobehind.godseye;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testBtn = (Button)findViewById(R.id.testBtn);
+
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, AddCCTVActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
